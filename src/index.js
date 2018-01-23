4e587a0d9271d0cc94aa5f5a583194e4f9dc9588
Yaroslav Cherednyk <azazely85@gmail.com>
2018-01-23 14:32:43 +0200

    JSX syntax

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-
+    btnOnClick(event) {
+        console.log(true, event.target);
+    }
     render() {
         return (
-            <div>
+            <div style={{backgroundColor: 'red'}}>
                 <h1>App ready to work </h1>
-                <h3>it is real react</h3>
+                <h3>it is real react </h3>
+                <button onClick={this.btnOnClick}>{ this.props.children }</button>
             </div>
         );
     }
 }
 ReactDOM.render(
-    <App />,
+    <App>Hello world</App>,
     document.getElementById('app')
 );
