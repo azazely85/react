a2a5a5c728ae6eef4e9fe0af93af413158b50661
Yaroslav Cherednyk <azazely85@gmail.com>
2018-01-23 13:12:22 +0200

    fist react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-let lilu = 'name';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>App ready to work </h1>
+                <h3>it is real react</h3>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
